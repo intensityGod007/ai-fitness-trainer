5b0b398f763e4b52454945507aa3efb783e5d09d
Hitesh Kumar <h.kumar96321@gmail.com>
2025-06-28 02:19:18 +0530

    setting up authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      Homepage
+      <SignedIn>
+        <SignOutButton/>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+    </div>
   )
 }
 
